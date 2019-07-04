037153dc004c085604331f8f61489c5f92f67857
Tamás Viczián <viczian.t@gmail.com>
2019-07-04 08:59:10 +0200

    initial commit

---
+++ src/main/java/hu/flowacademy/FeriSaidSo/FeriSaidSoApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.FeriSaidSo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriSaidSoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriSaidSoApplication.class, args);
+	}
+
+}
